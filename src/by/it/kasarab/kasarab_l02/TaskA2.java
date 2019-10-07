9c0bf0e4ba07ed1d1e22bbd2ec29da5baec34878
kasarabtanya <kasarabtanya@gmail.com>
2019-10-07 20:38:37 +0300

    lesson 02

---
+++ src/by/it/kasarab/kasarab_l02/TaskA2.java
@@ -0,0 +1,22 @@
+package by.it.kasarab.kasarab_l02;
+
+/*
+напишите тело main программы которая выводит на экран 5 строк
+"Я начинаю изучать Java!"
+
+Для ручной проверки запустите программу Ctrl+Shift+F10
+Для автоматической проверки откройте и запустите класс Testing
+*/
+
+class TaskA2 {
+    public static void main(String[] args){
+        String s = "Я начинаю изучать Java!";
+  int c;
+  for (c=0; c<5; c=c+1)
+System.out.println (s);
+
+}
+
+}
+
+
