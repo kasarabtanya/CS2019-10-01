8edb4060c371b9feb2a9a35a21ca9a91eb102595
user <bns223@mail.ru>
2019-10-17 11:42:06 +0300

    lesson 06 start

---
+++ src/by/it/kasarab/lesson06/TaskA1.java
@@ -0,0 +1,52 @@
+package by.it.kasarab.lesson06;
+
+/*
+Геттеры и сеттеры для класса Dog
+
+Создайте class Dog.
+У собаки должна быть кличка String name и возраст int age.
+Создайте геттеры и сеттеры для всех переменных класса Dog.
+
+        Требования:
+        1. Программа не должна считывать данные с клавиатуры.
+        2. У класса Dog должна быть переменная name с типом String.
+        3. У класса Dog должна быть переменная age с типом int.
+        4. У класса должен сеттер для переменной name.
+        5. У класса должен геттер для переменной name.
+        6. У класса должен сеттер для переменной age.
+        7. У класса должен геттер для переменной age.
+        8. Создайте внутри метода main (класса TaskA1) две разных собаки (т.е. два объекта типа Dog)
+        9. Заполните поля собак используя сеттеры.
+            Первая - Шарик, 5 лет. Вторая - Тузик, 3 года.
+        10. Напечатайте этих двух собак, выводите собак в формате
+            Кличка Возраст
+            через пробел. При выводе обязательно иcпользуйте геттеры.
+
+Ожидается вывод:
+Шарик 5
+Тузик 3
+*/
+
+/*
+
+public class TaskA1 {
+  public  class Dog {
+     String name;
+     int age;
+
+     public String getName(){  return name;   }
+
+     public void setName(String name){ this.name=name;   }
+
+     public int getAge(int age){  return age;  }
+
+ public int setAge (int age){  return age;  }
+
+    public static void main(String[] args) {
+      Dog dog1=new Dog("Шарик",5);
+      Dog dog2=new Dog("Тузик",3);
+      System.out.println(dog1.setName()+dog1.getName() );
+        System.out.println(dog2.setName()+dog2.getName() );
+    }
+} }
+*/
