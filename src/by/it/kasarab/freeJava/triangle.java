76aaf4ac5a7810ec0cf8d25e1e83baf09f7af907
kasarabtanya <kasarabtanya@gmail.com>
2019-11-27 16:48:41 +0300

    free

--- src/by/it/kasarab/freeJava/triangle.java
+++ src/by/it/kasarab/freeJava/triangle.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class triangle {
     public static void main(String[] args) {
+        System.out.println("Введите 3 целых числа от 1 до 1000");
         int a = 0;
         int b = 0;
         int c = 0;
@@ -11,28 +12,17 @@ public class triangle {
         a = sc.nextInt();
         b = sc.nextInt();
         c = sc.nextInt();
-        if (a < 1 || a > 1000) {
-            System.out.println("Сторона А не соответсвует условиям");
-        } else if ((b + c) <= a) {
-            System.out.println("Сторона A слишком велика для построения треугольника");
-        } else if (b < 1 || b > 1000) {
-            System.out.println("Сторона B не соответсвует условиям");
-        } else if ((a + c) <= b) {
-            System.out.println("Сторона B слишком велика для построения треугольника");
-        } else if (c < 1 || c > 1000) {
-            System.out.println("Сторона C не соответсвует условиям");
-        } else if ((a + b) <= c) {
-            System.out.println("Сторона C слишком велика для построения треугольника");
-        } else if (a == b && a != c)
+        if ((a < 1 || a > 1000) || (b < 1 || b > 1000) || (c < 1 || c > 1000)) {
+            System.out.println("Стороны должны быть от 1 до 1000");
+        } else if ((b + c <= a) || ((a + c) <= b) || ((a + b) <= c)) {
+            System.out.println("Одна из сторон слишком велика для построения треугольника");
+        } else if ((a == b && a != c) || (a != c && b == c) || (a == c && b != c))
             System.out.println("Треугольник равнобедренный");
         else if (a == b && a == c)
             System.out.println("Треугольник равносторонний");
-        else if (a != c && b == c)
-            System.out.println("Треугольник равнобедренный");
         else if (a != c && b != c)
             System.out.println("Треугольник обычный");
-        else if (a == c && b != c)
-            System.out.println("Треугольник равнобедренный");
+
     }
 }
 
